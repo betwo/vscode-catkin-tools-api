66f7c73c9050d049df6ca1f641a0ff1848a75339
Sebastian Buck <mail@betwo.eu>
2022-02-15 01:15:50 +0100

    Replace test explorer API with native testing

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,74 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import { SourceFileConfiguration } from 'vscode-cpptools';
-import { TestSuiteInfo, TestInfo } from 'vscode-test-adapter-api';
 
 export const vscodeCatkinToolsExtensionId = 'betwo.catkin-tools';
 
 export const VERSION = "1.1.0";
+
+/**
+ * Information about a test suite.
+ */
+export interface TestSuiteInfo {
+    type: 'suite';
+    id: string;
+    /** The label to be displayed by the Test Explorer for this suite. */
+    label: string;
+    /** The description to be displayed next to the label. */
+    description?: string;
+    /** The tooltip text to be displayed by the Test Explorer when you hover over this suite. */
+    tooltip?: string;
+    /**
+     * The file containing this suite (if known).
+     * This can either be an absolute path (if it is a local file) or a URI.
+     * Note that this should never contain a `file://` URI.
+     */
+    file?: string;
+    /** The line within the specified file where the suite definition starts (if known). */
+    line?: number;
+    /** Set this to `false` if Test Explorer shouldn't offer debugging this suite. */
+    debuggable?: boolean;
+    children: (TestSuiteInfo | TestInfo)[];
+    /** Set this to `true` if there was an error while loading the suite */
+    errored?: boolean;
+    /**
+     * This message will be displayed by the Test Explorer when the user selects the suite.
+     * It is usually used for information about why the suite was set to errored.
+     */
+    message?: string;
+}
+/**
+ * Information about a test.
+ */
+export interface TestInfo {
+    type: 'test';
+    id: string;
+    /** The label to be displayed by the Test Explorer for this test. */
+    label: string;
+    /** The description to be displayed next to the label. */
+    description?: string;
+    /** The tooltip text to be displayed by the Test Explorer when you hover over this test. */
+    tooltip?: string;
+    /**
+     * The file containing this test (if known).
+     * This can either be an absolute path (if it is a local file) or a URI.
+     * Note that this should never contain a `file://` URI.
+     */
+    file?: string;
+    /** The line within the specified file where the test definition starts (if known). */
+    line?: number;
+    /** Indicates whether this test will be skipped during test runs */
+    skipped?: boolean;
+    /** Set this to `false` if Test Explorer shouldn't offer debugging this test. */
+    debuggable?: boolean;
+    /** Set this to `true` if there was an error while loading the test */
+    errored?: boolean;
+    /**
+     * This message will be displayed by the Test Explorer when the user selects the test.
+     * It is usually used for information about why the test was set to errored.
+     */
+    message?: string;
+}
 
 export interface API {
     reload(): Promise<void>;
@@ -43,13 +106,13 @@ export interface IWorkspace {
         outline_only: boolean,
         build_dir?: String,
         devel_dir?: String):
-        Promise<WorkspaceTestSuite>
+        Promise<WorkspaceTestSuite>;
     locatePackageXML(package_name: String): void;
     buildDependencyGraph(): void;
     iteratePossibleSourceFiles(
         file: vscode.Uri,
         async_filter: (uri: vscode.Uri) => Promise<boolean>
-    ): void
+    ): void;
     iterateDependentPackages(
         workspace_package: IPackage,
         resursive_search: boolean,
